0114a0dd5a2ae8256dee1a89897bc417dde82827
Samar Singh <112770584+SamarS1ngh@users.noreply.github.com>
2024-06-08 11:21:33 +0530

    Delete lib/Utils/provider directory

--- lib/Utils/provider/app_providers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../features/home/model/home.dart';
-
-final counterProvider = StateNotifierProvider<HomeChangenotifierClass, int>(
-    (ref) => HomeChangenotifierClass());
